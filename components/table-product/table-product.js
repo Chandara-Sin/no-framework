a005eecb95f4f2f82812f5b5f3d635c3c0e830eb
Chandara-Sin <cdsin68@gmail.com>
2022-12-25 15:08:15 +0700

    feat: add table product component

---
+++ components/table-product/table-product.js
@@ -0,0 +1,63 @@
+class TableProduct extends HTMLElement {
+  static get observedAttributes() {
+    return ["products"];
+  }
+
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const table = document.createElement("table");
+    table.setAttribute("id", "product-table");
+    table.createTBody();
+    const tHead = table.createTHead();
+    const headerRow = tHead.insertRow();
+    headerRow.insertCell().outerHTML = "<th>Name</th>";
+    headerRow.insertCell().outerHTML = "<th>Price</th>";
+
+    shadow.appendChild(table);
+  }
+
+  attributeChangedCallback(changedAttribute) {
+    if (changedAttribute === "products") {
+      this.render(JSON.parse(this.getAttribute("products")));
+    }
+  }
+
+  render = (products) => {
+    const table = this.shadowRoot.getElementById("product-table");
+    for (const product of new Set(
+      products.map((product) => product.category)
+    )) {
+      this.addSectionHeader(table, product);
+      const productsInCat = products.filter((p) => p.category === product);
+      this.addProducts(table, productsInCat);
+    }
+  };
+
+  addSectionHeader = (t, category) => {
+    const row = t.insertRow();
+    const th = document.createElement("th");
+    th.setAttribute("colspan", 2);
+    th.innerText = category;
+    row.appendChild(th);
+  };
+
+  addProducts = (t, products) => {
+    for (const product of products) {
+      const row = t.insertRow();
+
+      const nameTd = document.createElement("td");
+      nameTd.innerText = product.name;
+      const priceTd = document.createElement("td");
+      priceTd.innerText = product.price;
+
+      if (!product.stocked) {
+        nameTd.style.color = "red";
+      }
+      row.appendChild(nameTd);
+      row.appendChild(priceTd);
+    }
+  };
+}
+
+customElements.define("table-product", TableProduct);

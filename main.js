38c5aefdd902085e4729be0654a19ded53c57f14
Chandara-Sin <cdsin68@gmail.com>
2022-12-25 15:32:42 +0700

    refactor: clean code

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
+let PRODUCTS = [];
+
+async function fetchProducts() {
+  try {
+    const response = await fetch("products.json");
+    PRODUCTS = await response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
 const table = document.getElementById("product-table");
 const searchInput = document.getElementById("search-input");
 const isstockedOnly = document.getElementById("is-stocked-only");
 
+fetchProducts()
+  .then(() => table.setAttribute("products", JSON.stringify(PRODUCTS)))
+  .catch((e) => console.error(e));
+
 searchInput.addEventListener("input", (e) => {
   searchValue = e.target.value;
-  render();
+  handleProductsChange();
 });
 
 isstockedOnly.addEventListener("change", (e) => {
   isStocked = e.target.checked;
-  render();
+  handleProductsChange();
 });
 
 let searchValue = "";
 let isStocked = false;
 
-function render() {
+function handleProductsChange() {
   const filteredProducts = PRODUCTS.filter((p) =>
     p.name.startsWith(searchValue)
   ).filter((p) => (isStocked ? p.stocked : true));
